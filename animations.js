0fe4a498bd5d01c029ea9a1fcc77c492e218c991
Ali Leon <alileon@mood-agency.com>
2022-06-01 12:37:37 -0400

    Actualización de cambios en resolución Issues

--- animations.js
+++ animations.js
@@ -22,7 +22,7 @@ $(window).scroll(function () {
     var top_of_screen = $(window).scrollTop();
 
     if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-        $('.intro-box').addClass('animate__animated animate__fadeInUp');
+        $('.intro-box').addClass('animate__animated animate__fadeInUp animate__delay-1s');
     } /* else {
         $('.intro-box').removeClass('animate__animated animate__fadeInUp');
     } */
@@ -37,7 +37,7 @@ $(window).scroll(function () {
     var top_of_screen = $(window).scrollTop();
 
     if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-        $('#p-img').addClass('animate__animated animate__fadeIn');
+        $('#p-img').addClass('animate__animated animate__fadeIn animate__delay-2s');
     } /* else {
         $('#p-img').removeClass('animate__animated animate__fadeIn');
     } */
@@ -51,7 +51,7 @@ $(window).scroll(function () {
     var top_of_screen = $(window).scrollTop();
 
     if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-        $('.detail-box').addClass('animate__animated animate__fadeInRight');
+        $('.detail-box').addClass('animate__animated animate__fadeInRight animate__delay-1s');
     } /* else {
         $('.detail-box').removeClass('animate__animated animate__fadeInRight');
     } */
@@ -65,7 +65,7 @@ $(window).scroll(function () {
     var top_of_screen = $(window).scrollTop();
 
     if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-        $('#backg-img').addClass('animate__animated animate__fadeIn');
+        $('#backg-img').addClass('animate__animated animate__fadeIn animate__delay-1s');
     } /* else {
         $('#backg-img').removeClass('animate__animated animate__fadeIn');
     } */
@@ -79,7 +79,23 @@ $(window).scroll(function () {
     var top_of_screen = $(window).scrollTop();
 
     if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-        $('#detail-box').addClass('animate__animated animate__fadeInRight');
+        $('#detail-box').addClass('animate__animated animate__fadeInRight animate__delay-1s');
+    } /* else {
+        $('.detail-box').removeClass('animate__animated animate__fadeInRight');
+    } */
+});
+
+/* Animation 6 */
+$(window).scroll(function () {
+    var top_of_element = $("#solar-panels-image").offset().top;
+    var bottom_of_element = $("#solar-panels-image").offset().top + $("#solar-panels-image").outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+        $('#left-wing').addClass('animate__animated animate__slideInRight animate__delay-1s');
+        $('#center-device').addClass('animate__animated animate__fadeIn animate__faster');
+        $('#right-wing').addClass('animate__animated animate__slideInLeft animate__delay-1s');
     } /* else {
         $('.detail-box').removeClass('animate__animated animate__fadeInRight');
     } */
